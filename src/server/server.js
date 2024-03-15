654f3ad7fc776f1b0eae92612f5ae02850265f2d
Goico <tomasgoico7@gmail.com>
2024-03-15 16:32:52 -0300

    Servidor web donde sirve una web

---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const startServer = (options) => {
+    const {port, public_path = 'public'} = options
+    const app = express()
+    //Usamos use para usar middlewares
+    app.use(express.static(public_path))//Contenido estatico para que se use
+    app.get('*',(req, res) => {
+        const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+
+    app.listen(port, () => {
+        console.log( `Escuchando en el puerto ${port}`)
+    })
+}
+
+module.exports = {
+    startServer
+}
